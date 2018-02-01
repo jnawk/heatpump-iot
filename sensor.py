0269a695de5614884dfb29d89b280f73bf464f00
Philip Dowie <philip@jnawk.nz>
2018-02-01 21:47:00 +1300

    changed sensor logic

--- sensor.py
+++ sensor.py
@@ -5,6 +5,9 @@ from numpy import median
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 
+ON = 1
+OFF = 0
+
 class Sensor(object):
     """Sensor class"""
     def __init__(self,
@@ -15,33 +18,105 @@ class Sensor(object):
         self.sensor = sensor
         self.data_pin = data_pin
         self.onoff_pin = onoff_pin
+        self._sensor_state = OFF
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.onoff_pin, GPIO.OUT)
 
         atexit.register(GPIO.cleanup)
 
-    def _boot_sensor(self):
-        GPIO.output(self.onoff_pin, 1)
-        time.sleep(2)
+    @property
+    def sensor_state(self):
+        """State of sensor"""
+        return self._sensor_state
 
-    def _shutdown_sensor(self):
-        GPIO.output(self.onoff_pin, 0)
+    @sensor_state.setter
+    def sensor_state(self, sensor_state):
+        if self._sensor_state == sensor_state:
+            return
 
-    def read(self):
+        if sensor_state != ON and sensor_state != OFF:
+            raise ValueError('Sensor state can only be ON or OFF')
+
+        GPIO.output(self.onoff_pin, sensor_state)
+        self._sensor_state = sensor_state
+        if sensor_state == ON:
+            time.sleep(2)
+
+    @property
+    def current_sample(self):
+        """Reads the sensor and returns the current samples"""
+        old_sensor_state = self.sensor_state
+        try:
+            self.sensor_state = ON
+            return Adafruit_DHT.read_retry(self.sensor, self.data_pin)
+        finally:
+            self.sensor_state = old_sensor_state
+
+    @property
+    def sample(self):
         """read the sensor"""
         try:
-            self._boot_sensor()
-            temperature_samples = []
-            humidity_samples = []
-            tries = 10
-            while tries > 0 and len(temperature_samples) < 3 and len(humidity_samples) < 3:
-                humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.data_pin)
-                if temperature is not None:
-                    temperature_samples.append(temperature)
-                if humidity is not None:
-                    humidity_samples.append(humidity)
-            return (round(median(humidity_samples), 1),
-                    round(median(temperature_samples), 1))
+            # turn on sensor so the sampler doesn't turn it off
+            self.sensor_state = ON
+            samples = Samples()
+            tries_remaining = 10
+            while tries_remaining > 0 and samples.sample_count < 3:
+                samples.humidity, samples.temperature = self.current_sample
+                tries_remaining = tries_remaining - 1
+            return Sample(samples.humidity, samples.temperature)
         finally:
-            self._shutdown_sensor()
+            self.sensor_state = OFF
+
+class Sample(object):
+    """Sample class"""
+    def __init__(self, humidity, temperature):
+        self._humidity = humidity
+        self._temperature = temperature
+
+    @property
+    def humidity(self):
+        """The humidity"""
+        return self._humidity
+
+    @property
+    def temperature(self):
+        """The temperature"""
+        return self._temperature
+
+class Samples(object):
+    """Samples class"""
+    def __init__(self):
+        self._temperature = []
+        self._humidity = []
+
+    @property
+    def sample_count(self):
+        """Minimum number of samples collected"""
+        return min(len(self._temperature), len(self._humidity))
+
+    @property
+    def temperature(self):
+        """Average temperature"""
+        if not self._temperature:
+            return None
+
+        return round(median(self._temperature))
+
+    @property
+    def humidity(self):
+        """Average humidity"""
+        if not self._humidity:
+            return None
+
+        return round(median(self._humidity))
+
+    @temperature.setter
+    def temperature(self, temperature):
+        if temperature is not None:
+            self._temperature.append(temperature)
+
+    @humidity.setter
+    def humidity(self, humidity):
+        if humidity is not None:
+            self._humidity.append(humidity)
