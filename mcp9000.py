b3212432e043a10832d47e59261d56f8b897bd46
Philip Dowie <philip@jnawk.nz>
2019-07-11 21:06:03 +1200

    added try catch around i2c read

--- mcp9000.py
+++ mcp9000.py
@@ -17,6 +17,8 @@ class MCP9000(object): # pylint: disable=too-few-public-methods
     def temperature(self):
         """Current temperature"""
         self.bus.write_byte(self.address, 0x00)
-        data = self.bus.read_i2c_block_data(self.address, 0x00, 2)
-
-        return data[0] * 16 + data[1] / 16.0
+        try:
+            data = self.bus.read_i2c_block_data(self.address, 0x00, 2)
+            return data[0] * 16 + data[1] / 16.0
+        except IOError:
+            return None
